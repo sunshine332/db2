b917200e715b6bf6eb0c6df67cd264a58093167b
zhangsan <18107024178@163.com>
2020-04-30 17:41:20 +0800

    bbbbbb

---
+++ mystudy/src/demo/Meth.java
@@ -0,0 +1,14 @@
+package demo;
+
+
+
+public class Meth {
+    public static void main(String[] args) {
+        System.out.println("hrlloworld!!!");
+        System.out.println("hrlloworld!!!");
+        System.out.println("hrlloworld!!!");
+        System.out.println("hrlloworld!!!");
+        System.out.println("hrlloworld!!!");
+        System.out.println("hrlloworld!!!");
+    }
+}
